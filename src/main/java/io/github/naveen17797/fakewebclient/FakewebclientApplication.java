46b6804ebb6adeb0318c5db4847f679e262a9828
naveen <kmnaveen101@gmail.com>
2021-10-24 14:45:35 +0530

    bump to 0.0.1

--- src/main/java/io/github/naveen17797/fakewebclient/FakewebclientApplication.java
+++ src/main/java/io/github/naveen17797/fakewebclient/FakewebclientApplication.java
@@ -7,12 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 public class FakewebclientApplication {
 
-    private WebClient.Builder builder;
-
-    void foo() {
-        builder.build().post().exchange();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(FakewebclientApplication.class, args);
 
