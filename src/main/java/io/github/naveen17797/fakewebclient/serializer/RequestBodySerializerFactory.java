81255eeaf707ad044584f398a43511dfc7b89659
naveen <kmnaveen101@gmail.com>
2021-10-31 16:24:44 +0530

    Make serializer return a singleton

--- src/main/java/io/github/naveen17797/fakewebclient/serializer/RequestBodySerializerFactory.java
+++ src/main/java/io/github/naveen17797/fakewebclient/serializer/RequestBodySerializerFactory.java
@@ -1,9 +1,16 @@
 package io.github.naveen17797.fakewebclient.serializer;
 
+import java.util.Optional;
+
 public class RequestBodySerializerFactory {
 
+    private static Optional<RequestBodySerializer> instance = Optional.empty();
+
     public static RequestBodySerializer getInstance() {
-        return new DefaultRequestBodySerializer();
+        if (instance.isEmpty()) {
+            instance = Optional.of(new DefaultRequestBodySerializer());
+        }
+        return instance.get();
     }
 
 }
